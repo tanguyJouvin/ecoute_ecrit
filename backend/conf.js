21d6430f85e1a35c9b81c37bee1f80fa319ae3c0
Tanguy Jouvin <42809139+tanguyJouvin@users.noreply.github.com>
2020-02-04 09:30:42 +0100

    Delete conf.js

--- backend/conf.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'soleil',
-  database: 'ecoute_ecrit',
-  multipleStatements: true
-});
-module.exports = connection;
